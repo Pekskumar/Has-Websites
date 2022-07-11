c69ed1ccd28911c0b8d01317ecab2943c37a818a
Pekskumar <79084617+Pekskumar@users.noreply.github.com>
2022-07-11 23:33:47 +0530

    add typing number js and download resume button

--- js/script.js
+++ js/script.js
@@ -156,6 +156,49 @@ $(function() {
         scrollTop: jQuery(".contact-one").offset().top
         }, 2000);
         });
+
+        // typing name
+        var span = document.querySelector(".typewriter span");
+var textArr = span.getAttribute("data-text").split(", "); 
+var maxTextIndex = textArr.length; 
+
+var sPerChar = 0.15; 
+var sBetweenWord = 1.5;
+var textIndex = 0; 
+
+typing(textIndex, textArr[textIndex]); 
+
+function typing(textIndex, text) {
+    var charIndex = 0; 
+    var maxCharIndex = text.length - 1; 
+    
+    var typeInterval = setInterval(function () {
+        span.innerHTML += text[charIndex]; 
+        if (charIndex == maxCharIndex) {
+            clearInterval(typeInterval);
+            setTimeout(function() { deleting(textIndex, text) }, sBetweenWord * 1000); 
+            
+        } else {
+            charIndex += 1; 
+        }
+    }, sPerChar * 1000); 
+}
+
+function deleting(textIndex, text) {
+    var minCharIndex = 0; 
+    var charIndex = text.length - 1; 
+
+    var typeInterval = setInterval(function () {
+        span.innerHTML = text.substr(0, charIndex); 
+        if (charIndex == minCharIndex) {
+            clearInterval(typeInterval);
+            textIndex + 1 == maxTextIndex ? textIndex = 0 : textIndex += 1; 
+            setTimeout(function() { typing(textIndex, textArr[textIndex]) }, sBetweenWord * 1000); 
+        } else {
+            charIndex -= 1; 
+        }
+    }, sPerChar * 1000); 
+}
    
    // Mouse cursor
   let prim = document.querySelector ('#cursor .primary');
@@ -467,14 +510,14 @@ $(function(){
              slidesToShow: 2,
              slidesToScroll: 1
           }
-         },
-          {
-           breakpoint: 480,
-           settings: {
-             speed: 5000,
-             slidesToShow: 1,
-             slidesToScroll: 1
-           }
+        //  },
+        //   {
+        //    breakpoint: 480,
+        //    settings: {
+        //      speed: 5000,
+        //      slidesToShow: 1,
+        //      slidesToScroll: 1
+        //    }
          }]
    });
 });
